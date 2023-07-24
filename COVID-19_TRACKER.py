3a0cf857b454b6a3e8abfa277a19afd84e8e77b4
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-07-24 19:18:37 +0800

    Initialize class attributes

--- COVID-19_TRACKER.py
+++ COVID-19_TRACKER.py
@@ -22,3 +22,26 @@ class CovidContactTracingApp:
         if not consent:
             # If the user declines consent, exit the application
             return
+        
+        #Initialize class attributes
+        self.notebook = ttk.Notebook(self.window)
+        self.notebook.pack(expand=True, fill=tk.BOTH)
+        
+        self.personal_info_frame = ttk.Frame(self.notebook)
+        self.emergency_frame = ttk.Frame(self.notebook)
+        self.questions_frame = ttk.Frame(self.notebook)
+
+        self.notebook.add(self.personal_info_frame, text="Personal Info")
+        self.notebook.add(self.emergency_frame, text="Emergency Contact")
+        self.notebook.add(self.questions_frame, text="Health Questions")
+
+        self.create_personal_info_tab()
+        self.create_emergency_contact_tab()
+        self.create_questions_tab()
+
+    def run(self):
+        self.window.mainloop()
+
+if __name__ == "__main__":
+    app = CovidContactTracingApp()
+    app.run()
